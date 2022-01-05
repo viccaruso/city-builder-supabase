e8e4112d46d0c46c0b76f8038f6220454d45550f
Victor Caruso <vic.caruso@gmail.com>
2022-01-05 10:51:52 -0800

    dropdowns and mottos working

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -22,10 +22,10 @@ logoutButton.addEventListener('click', () => {
     logout();
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // On page load attempt to fetch city from supabase
     city = await fetchCity();
-    
+
     // Check if fetch was successful and if not, create row in supabase with default values and store the returned default city in state
     if (!city) {
         city = await createDefaultCity();
@@ -35,12 +35,51 @@ window.addEventListener('load', async() => {
     displayCity(city);
 });
 
-setNameButton.addEventListener('click', async() => {
+setNameButton.addEventListener('click', async () => {
     // Grab the name from input field and then clear field
-    let name = cityInputEl.value;
+    let data = cityInputEl.value;
     cityInputEl.value = '';
+    // Update name in db and update state
+    city = await updateCityDetail('name', data);
+    // Display fresh data (from state)
+    displayCity(city);
+});
+
+waterDropdown.addEventListener('change', async() => {
+    // Grab the value from dropdown
+    let data = waterDropdown.value;
     // Update detail in db and update state
-    city = await updateCityDetail('name', name);
+    city = await updateCityDetail('water_type', data);
+    // Display fresh data (from state)
+    displayCity(city);
+});
+
+densityDropdown.addEventListener('change', async() => {
+    // Grab the value from dropdown
+    let data = densityDropdown.value;
+    // Update detail in db and update state
+    city = await updateCityDetail('density_type', data);
+    // Display fresh data (from state)
+    displayCity(city);
+});
+
+parkDropdown.addEventListener('change', async() => {
+    // Grab the value from dropdown
+    let data = parkDropdown.value;
+    // Update detail in db and update state
+    city = await updateCityDetail('park_type', data);
+    // Display fresh data (from state)
+    displayCity(city);
+});
+
+addMottoButton.addEventListener('click', async() => {
+    // Grab motto from input field and clear field
+    let data = mottoInput.value;
+    mottoInput.value = '';
+    // Add motto to array in state
+    city.mottos.push(data);
+    // Update mottos in db and also update state
+    city = await updateCityDetail('mottos', city.mottos);
     // Display fresh data (from state)
     displayCity(city);
 });
@@ -61,5 +100,16 @@ function displayCity(city) {
     parkDropdown.value = city.park_type;
 
     // Display mottos
+        // Remove all mottos from DOM
+    while (mottoContainerEl.firstChild) {
+        mottoContainerEl.firstChild.remove();
+    }
+        // Add all mottos back to DOM
+    for (let motto of city.mottos) {
+        let p = document.createElement('p');
+        p.textContent = motto;
+        p.style.textAlign = 'center';
+        mottoContainerEl.append(p);
+    }
 
 }
